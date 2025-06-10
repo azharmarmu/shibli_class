05b1de2873cffdd1ab065f29d857f70e022c0818
azhar <marmuazhar@gmail.com>
2025-06-10 21:18:19 +0530

    Topic: Array List

--- src/teaching/array/ArrayJava.java
+++ src/teaching/array/ArrayJava.java
@@ -12,37 +12,11 @@ public class ArrayJava {
         int[] a = {5, 3, 4, 1, 2};
 
 
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Enter the value to be searched: ");
-            int searchValue = scanner.nextInt(); //10
-            searchArray(a, searchValue); //method call
-        }
+//a[3];
+
 
     }
 
     int a = 10;
-
-
-    double methodName1() {
-         methodName1();
-        return 10;
-    }
-
-
-    //method declaration
-    static void searchArray(int[] array, int searchValue) {
-        boolean matchFound = false;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == searchValue) {
-                System.out.println("Found value " + searchValue + " @ index: " + i);
-                matchFound = true;
-                break;
-            }
-        }
-        if (!matchFound) {
-            System.out.println("Not Found value " + searchValue);
-        }
-    }
 }
--- src/teaching/array/ArrayListJava.java
+++ src/teaching/array/ArrayListJava.java
@@ -1,4 +1,54 @@
 package teaching.array;
 
+import java.util.ArrayList;
+
 public class ArrayListJava {
+    public static void main(String[] args) {
+
+
+        //Declaration / Creation
+        ArrayList<String> names = new ArrayList<>() {{
+            add("Alice");
+            add("Bob");
+            add("Charlie");
+        }};
+
+        ArrayList<Integer> age = new ArrayList<>() {{
+            add(25);
+            add(18);
+            add(40);
+        }};
+
+        listIteration(age);
+        listIteration(names);
+
+//        names.add("Alex");
+//
+//
+//        names.remove("Bob"); //value based removal
+//        names.remove(2); //index based removal
+//
+//        names.add("New name");
+//
+//        boolean checkName = names.contains("Bob");
+//        System.out.println("CheckName: " + checkName);
+//
+//
+////        names.clear();
+//        boolean checkEmpty = names.isEmpty();
+//
+//        System.out.println("CheckEmpty: " + checkEmpty);
+//
+//        listIteration(names);
+    }
+
+
+    //Iteration
+    static void listIteration(ArrayList al) {
+        for (int i = 0; i < al.toArray().length; i++) {
+            System.out.println(i + "=> " + al.get(i));
+        }
+    }
+
+
 }
